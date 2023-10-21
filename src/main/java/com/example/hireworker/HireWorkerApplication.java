3b1960d91644596acaee9fa8a844837118bcdf1c
Ebrar Aydın <ebrarayd0606@gmail.com>
2023-10-21 15:29:01 +0300

    first commit of this project

---
+++ src/main/java/com/example/hireworker/HireWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hireworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HireWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hireworker/HireWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hireworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
